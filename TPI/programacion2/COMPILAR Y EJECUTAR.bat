aabc5cba1fc67904fdcbdb71b7163e1ca8f346f0
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-11-16 11:37:29 -0300

    comentarios en el bat de compilar

--- TPI/programacion2/COMPILAR Y EJECUTAR.bat
+++ TPI/programacion2/COMPILAR Y EJECUTAR.bat
@@ -1,4 +1,13 @@
 @echo off
+echo Se necesita Maven instalado para que funcione la compilacion
+echo. 
+echo Para compilar y que conecte a la base de datos
+echo se debe modificar el db.resources en TPI\programacion2\target\classes
+echo. 
+echo Para mas informacion, leer el README 
+echo. 
+pause
+
 REM Cambiar la página de códigos a UTF-8
 chcp 65001 >nul
 
