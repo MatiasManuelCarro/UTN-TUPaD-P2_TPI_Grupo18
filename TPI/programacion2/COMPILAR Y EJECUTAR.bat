c2fa2154601d00d04cbb82efd784fe5eb7ed9944
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2025-11-17 00:55:23 -0300

    version 1.0 final

--- TPI/programacion2/COMPILAR Y EJECUTAR.bat
+++ TPI/programacion2/COMPILAR Y EJECUTAR.bat
@@ -2,8 +2,11 @@
 echo Se necesita Maven instalado para que funcione la compilacion
 echo. 
 echo Para compilar y que conecte a la base de datos
+echo se debe modificar el db.resources en TPI\programacion2\src\main\resources
 echo se debe modificar el db.resources en TPI\programacion2\target\classes
 echo. 
+echo Si aparece un error de UTF-8 revisar en la carpeta Extras como configurar.
+echo.
 echo Para mas informacion, leer el README 
 echo. 
 pause
